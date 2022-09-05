8a8ae09b507c0928a670af4f29dd5c63aa26f4f2
Massimo <pesinmax@gmail.com>
2022-09-05 13:22:13 -0400

    partials - video 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildSass() {
-    return src('index.scss')
+    //return src('index.scss')
+    return src('*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchSass() {
-    watch(['index.scss'], buildSass)
+    //watch(['index.scss'], buildSass)
+    watch(['*.scss'], buildSass)
+
 }
 
 exports.default = series(buildSass, watchSass)
