f230716da094f92e334da89b0660e2ba82be0a96
Massimo <pesinmax@gmail.com>
2023-03-19 14:06:29 -0400

    lesson 20 - purging css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildSass() {
     //return src('index.scss')
     //return src('*.scss')
     return src('clientLibs/**/*.scss')
     .pipe(sass())
+    // used for purgecss plugin
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchSass() {
     //watch(['index.scss'], buildSass)
     //watch(['*.scss'], buildSass)
-    watch(['clientLibs/**/*.scss'], buildSass)
+    // watch scss and all html tiles
+    watch(['clientLibs/**/*.scss', '*.html'], buildSass)
 
 }
 
